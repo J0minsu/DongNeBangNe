e0262b4ac664ef74adecee05b9218c7dd3771d9e
minsuJo <tok0419@naver.com>
2019-10-04 21:21:28 +0900

    Join 후 DB 저장하는 방법을 @Transient 로 선언 된 맴버변수에 저장할 수 있다는 것을 학습. Cart API 작업 중(get)
    컨디션 정상적을 회복.

--- src/main/java/com/silort/swm/controller/CartController.java
+++ src/main/java/com/silort/swm/controller/CartController.java
@@ -1,6 +1,6 @@
 package com.silort.swm.controller;
 
-import java.util.Map;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.silort.swm.model.Cart;
 import com.silort.swm.model.Order;
+import com.silort.swm.model.Product;
 import com.silort.swm.model.User;
 import com.silort.swm.repo.CartRepository;
 import com.silort.swm.repo.OrderRepository;
+import com.silort.swm.repo.ProductRepository;
 import com.silort.swm.repo.UserRepository;
 
 @RestController
@@ -30,12 +33,15 @@ public class CartController {
 
 	@Autowired
 	private CartRepository cartRepository;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
+
+	@Autowired
+	private ProductRepository productRepository;
 
 	@GetMapping(value = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Cart> findCartByUserId(@PathVariable int userId) {
@@ -52,43 +58,99 @@ public class CartController {
 		logger.debug("Calling postCart( )");
 
 		Cart postCart = new Cart(userId, 0);
-		
+
 		cartRepository.save(postCart);
-		
+
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
-	
+
 	@PostMapping(value = "/buying/{userId}")
 	public ResponseEntity<Void> buyingCart(@PathVariable int userId) {
-		
+
 		logger.debug("Calling buyingCart( )");
-		
+
 		Cart buyingCart = cartRepository.findCartById(userId);
 		User user = userRepository.findUserById(userId);
-		
+
 		user.setBalance(user.getBalance() - buyingCart.getPrice());
-		
+
 		userRepository.save(user);
-		
-		orderRepository.save(new Order(userId, buyingCart.getPrice(),
-				buyingCart.getProduct01Id(), buyingCart.getProduct01Quantity(),
-				buyingCart.getProduct02Id(), buyingCart.getProduct02Quantity()));
-		
-		
+
+		orderRepository.save(new Order(userId, buyingCart.getPrice(), buyingCart.getProduct01Id(),
+				buyingCart.getProduct01Quantity(), buyingCart.getProduct02Id(), buyingCart.getProduct02Quantity()));
+
 		resetCart(buyingCart);
-		
+
 		cartRepository.save(buyingCart);
-		
+
 		return new ResponseEntity<Void>(HttpStatus.OK);
-		
+
 	}
-	
-	@PostMapping(value = "carts/add/{userId}{productId}/{productQ}")
-	public ResponseEntity<Void> addIteminCart(@RequestParam Map<Integer, String> parameters) {
+
+	@PostMapping(value = "carts/add")
+	public ResponseEntity<String> addIteminCart(@RequestParam List<String> parameters) {
+		//[0] userId, [1] productId, [2] productQuantity
+
+		int userId = Integer.parseInt(parameters.get(0));
+		int productId = Integer.parseInt(parameters.get(1));
+		int productQ = Integer.parseInt(parameters.get(2));
 		
-		return new ResponseEntity<Void>(HttpStatus.OK);
+		Cart cart =cartRepository.findCartById(userId);
+		Product product = productRepository.findById(productId);
+		
+		String msg = "success";
+		
+		if(cart.getProduct01Id() == 0) {
+			if(cart.getProduct02Id() == productId) {
+				cart.setProduct02Quantity(cart.getProduct02Quantity() + productQ);
+				cart.setPrice(cart.getPrice() + (product.getPrice() * productQ));
+				
+				return new ResponseEntity<String>(msg, HttpStatus.OK);
+			}
+			else {
+				cart.setProduct01Id(productId);
+				cart.setProduct01Quantity(productQ);
+				cart.setPrice(cart.getPrice() + (product.getPrice() * productQ));
+				
+				return new ResponseEntity<String>(msg, HttpStatus.OK);
+			}
+		}
+		else if(cart.getProduct01Id() == productId) {
+			cart.setProduct01Quantity(cart.getProduct01Quantity() + productQ);
+			cart.setPrice(cart.getPrice() + (product.getPrice() * productQ));
+			
+			return new ResponseEntity<String>(msg, HttpStatus.OK);
+		}
+		else {
+			if(cart.getProduct02Id() == productId)	{
+				cart.setProduct02Quantity(cart.getProduct02Quantity() + productQ);
+				cart.setPrice(cart.getPrice() + (product.getPrice() * productQ));
+				
+				return new ResponseEntity<String>(msg, HttpStatus.OK);
+			}
+			else if(cart.getProduct02Id() == 0) {
+				cart.setProduct02Id(productId);
+				cart.setProduct02Quantity(productQ);
+				cart.setPrice(cart.getPrice() + (product.getPrice() * productQ));
+				
+				return new ResponseEntity<String>(msg, HttpStatus.OK);
+			}
+			else {
+				return new ResponseEntity<String>(msg, HttpStatus.BAD_REQUEST);
+			}
+		}
 	}
-	
+
+	@DeleteMapping(value = "/userId")
+	public ResponseEntity<Void> clearCart(@PathVariable int userId) {
+
+		Cart cart = cartRepository.findCartById(userId);
+		resetCart(cart);
+
+		cartRepository.save(cart);
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+	}
+
 	private void resetCart(Cart cart) {
 		cart.setPrice(0);
 		cart.setProduct01Id(0);
--- src/main/java/com/silort/swm/controller/CartProductController.java
+++ src/main/java/com/silort/swm/controller/CartProductController.java
@@ -21,49 +21,50 @@ import com.silort.swm.model.CartProduct;
 import com.silort.swm.repo.CartProductRepository;
 import com.silort.swm.repo.CartRepository;
 
-@RestController
-@RequestMapping("api/cartProducts")
+//@RestController
+//@RequestMapping("api/cartProducts")
 public class CartProductController {
-
-	static Logger logger = LoggerFactory.getLogger(CartProductController.class);
-
-	@Autowired
-	private CartProductRepository cartProductRepository;
-
-	@Autowired
-	private CartRepository cartRepository;
-
-	@GetMapping(value = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<CartProduct>> findCartProductsByUserId(@PathVariable int userId) {
-		// he
-		logger.debug("Calling findCartProductsByUserId( )");
-
-		List<CartProduct> list = new ArrayList<CartProduct>();
-		Iterable<CartProduct> products = cartProductRepository.findCartProductByCartId(userId);
-		products.forEach(list::add);
-
-		return new ResponseEntity<List<CartProduct>>(list, HttpStatus.OK);
-	}
-
-	@PostMapping()
-	public ResponseEntity<Void> postCartProduct(@RequestParam CartProduct cartProduct) {
-		logger.debug("Calling postCartProduct( )");
-
-		CartProduct saveProduct = cartProductRepository.findCartProductByCartIdAndProductId(cartProduct.getCartId(),
-				cartProduct.getProductId());
-		if (saveProduct == null) {
-			saveProduct = new CartProduct(cartProduct.getProductId(), cartProduct.getCartId(),
-					cartProduct.getQuantity());
-		} else {
-			saveProduct.setQuantity(saveProduct.getQuantity() + cartProduct.getQuantity());
-		}
-
-		Cart cart = cartRepository.findCartById(cartProduct.getCartId());
-		cart.setPrice(cart.getPrice() + cartProduct.getQuantity() * cartProduct.getQuantity());
-
-		cartRepository.save(cart);
-		cartProductRepository.save(saveProduct);
-
-		return new ResponseEntity<Void>(HttpStatus.CREATED);
-	}
+//
+//	static Logger logger = LoggerFactory.getLogger(CartProductController.class);
+//
+//	@Autowired
+//	private CartProductRepository cartProductRepository;
+//
+//	@Autowired
+//	private CartRepository cartRepository;
+//
+//	@GetMapping(value = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+//	public ResponseEntity<List<CartProduct>> findCartProductsByUserId(@PathVariable int userId) {
+//		// he
+//		logger.debug("Calling findCartProductsByUserId( )");
+//
+//		List<CartProduct> list = new ArrayList<CartProduct>();
+//		Iterable<CartProduct> products = cartProductRepository.findCartProductByCartId(userId);
+//		products.forEach(list::add);
+//
+//		return new ResponseEntity<List<CartProduct>>(list, HttpStatus.OK);
+//	}
+//
+//	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+//	public ResponseEntity<Void> postCartProduct(@RequestParam CartProduct cartProduct) {
+//		logger.debug("Calling postCartProduct( )");
+//
+//		CartProduct saveProduct = cartProductRepository.findCartProductByCartIdAndProductId(cartProduct.getCartId(),
+//				cartProduct.getProductId());
+//		if (saveProduct == null) {
+//			saveProduct = new CartProduct(cartProduct.getProductId(), cartProduct.getCartId(),
+//					cartProduct.getQuantity());
+//		} else {
+//			saveProduct.setQuantity(saveProduct.getQuantity() + cartProduct.getQuantity());
+//		}
+//
+//		Cart cart = cartRepository.findCartById(cartProduct.getCartId());
+//		cart.setPrice(cart.getPrice() + cartProduct.getQuantity() * cartProduct.getQuantity());
+//
+//		cartRepository.save(cart);
+//		cartProductRepository.save(saveProduct);
+//
+//		return new ResponseEntity<Void>(HttpStatus.CREATED);
+//	}
+//	
 }
