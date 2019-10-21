8fe916f88d462a1721884aa1aa92d1d2aa66c916
minsuJo <tok0419@naver.com>
2019-10-21 18:22:22 +0900

    2차 배포, EC2 서버 실행 중
    Video 작성 중,
    
    Web Design 외주 구하는 중

--- src/main/java/com/silort/swm/controller/VideoController.java
+++ src/main/java/com/silort/swm/controller/VideoController.java
@@ -1,5 +1,46 @@
 package com.silort.swm.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.silort.swm.model.Video;
+import com.silort.swm.repo.VideoRepository;
+
+@RestController
+@RequestMapping("/api/videos")
 public class VideoController {
 
+	
+	static Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	@Autowired
+	private VideoRepository videoRepository;
+	
+//	@GetMapping
+//	public ResponseEntity<List<Video>> getVideos() {
+//	
+//		logger.debug("Call getVideos( )");
+//		List<Video> videos = videoRepository.findAllVideo();
+//		
+//		return new ResponseEntity<List<Video>>(videos, HttpStatus.OK);
+//	}
+	
+	@GetMapping(value = "/user/{userId}")
+	public ResponseEntity<List<Video>> getVideoByUploader(@PathVariable int userId) {
+		
+		logger.debug("Call getVideoByUploader( )");
+		
+		List<Video> videos = videoRepository.findVideosByUploaderId(userId);
+		
+		return new ResponseEntity<List<Video>>(videos, HttpStatus.OK);
+	}
 }
--- src/main/java/com/silort/swm/repo/VideoRepository.java
+++ src/main/java/com/silort/swm/repo/VideoRepository.java
@@ -1,9 +1,15 @@
 package com.silort.swm.repo;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.silort.swm.model.Video;
 
 public interface VideoRepository extends CrudRepository<Video, Integer> {
 
+	
+//	List<Video> findAllVideo();
+	
+	List<Video> findVideosByUploaderId(int userId);
 }
